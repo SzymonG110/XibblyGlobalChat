eaf77c1de40c3a7f87a09085562ee92c0fe7439e
SzymonG110 <gornikowskiszymon@gmail.com>
2022-07-16 08:28:05 +0200

    remove (all intents)

--- src/handlers/interaction.handler.ts
+++ src/handlers/interaction.handler.ts
@@ -9,25 +9,11 @@ export default class InteractionHandler {
         this.init()
     }
 
-    async init() {
+    async init(): Promise<void> {
 
         const command = bot.slashCommands.get(this.command.commandName)
 
-        if (!command) {
-            this.command.reply({
-                ephemeral: true,
-                embeds: [
-                    new Embed({
-                        title: 'Błąd',
-                        color: 'RED',
-                        content: 'Użyto nieistniejącego polecenia'
-                    })
-                ]
-            })
-            return console.error(`Unknown command: ${this.command.commandName}`)
-        }
-
-        if (command.globalchtMod && (!process.env.MODERATION_GC_TOKEN || !(process.env.MODERATION_GC_TOKEN != '')))
+        if (!command || command.globalchtMod && (!process.env.MODERATION_GC_TOKEN || !(process.env.MODERATION_GC_TOKEN != '')))
             return
 
         if (command.onlyGuild && !this.command.guild)
@@ -42,23 +28,17 @@ export default class InteractionHandler {
                 ]
             })
 
-        if (!this.command.guild?.members.cache.get(this.command.user.id)) {
-            !bot.users.cache.get(this.command.guild?.ownerId as string) && (await bot.users.fetch(this.command.user.id))
-
-            if (!this.command.guild?.members.cache.get(this.command.user.id))
-                return this.command.reply({
-                    ephemeral: true,
-                    embeds: [
-                        new Embed({
-                            title: 'Błąd',
-                            color: 'RED',
-                            content: 'Błąd którego nie znam! Sprobuj ponownie.'
-                        })
-                    ]
-                })
-        }
-
-        !this.command.guild?.name && (await this.command.guild?.fetch())
+        if (!this.command.guild?.members.cache.get(this.command.user.id))
+            return this.command.reply({
+                ephemeral: true,
+                embeds: [
+                    new Embed({
+                        title: 'Błąd',
+                        color: 'RED',
+                        content: 'Błąd którego nie znam! Sprobuj ponownie.'
+                    })
+                ]
+            })
 
         if (command.permissions && !new PermissionsUtil(command.permissions, this.command.member?.permissions as Permissions))
             return this.command.reply({
